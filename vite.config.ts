a23a848b40ba0827973c08233809ffc6f492a388
Arth Patel <patelarth93@gmail.com>
2026-04-02 10:15:52 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react(), tailwindcss()],
       build: {
-        chunkSizeWarningLimit: 1000,
+        chunkSizeWarningLimit: 3000,
         rollupOptions: {
           output: {
             manualChunks: {
